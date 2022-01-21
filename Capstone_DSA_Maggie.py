19658b026b4764a1ace1d32cf2a83fa2b3c5392b
maggiexio <53416822+maggiexio@users.noreply.github.com>
2022-01-21 17:07:14 -0500

    Update Capstone_DSA_Maggie.py

--- Capstone_DSA_Maggie.py
+++ Capstone_DSA_Maggie.py
@@ -12,7 +12,7 @@ def raw_data (input_file):
 st.title("Data Excursion for DSA Capstone Project")
 st.markdown("This Dashboard is designed for the capstone project of DSA 2021-2022")
 st.markdown ("By: Maggie Xiong") 
-st.latex(r'''R_{\mu\nu} - \frac{1}{2}Rg_{\mu\nu} = \frac{8\pi G}{c^4}T_{\mu\nu}''')
+#st.latex(r'''R_{\mu\nu} - \frac{1}{2}Rg_{\mu\nu} = \frac{8\pi G}{c^4}T_{\mu\nu}''')
          
          
 # read in data
@@ -22,9 +22,11 @@ with st.expander("Display the data"):
          st.dataframe(df_ori)
          
 #sidebar
-st.sidebar.markdown("## Filter the data")
-score_1, score_2 = st.sidebar.slider("please select score range", 0, 20, (0,20))    
+st.sidebar.markdown("## Define **filters:**")
+score_1, score_2 = st.sidebar.slider("Total score: ", 0, 20, (0,20))    
 df_1=df_ori.query("sum_score>=@score_1 and sum_score<=@score_2")
+time_1, time_2 = st.sidebar.slider("Total response time", 0, 500, (0,500))    
+df_1=df_ori.query("rt_total>=@time_1 and rt_total<=@time_2")
          
 st.markdown('##Data Visualizaion')
 fig_hist=px.histogram(df_1, x='sum_score', color='gender', facet_row='home_computer')
