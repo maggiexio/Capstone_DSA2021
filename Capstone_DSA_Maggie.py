23c81d1c702c37843fd5ccf5a022c15e237c5793
maggiexio <53416822+maggiexio@users.noreply.github.com>
2022-01-22 15:09:40 -0500

    Update Capstone_DSA_Maggie.py

--- Capstone_DSA_Maggie.py
+++ Capstone_DSA_Maggie.py
@@ -115,7 +115,7 @@ for i, state_ori in enumerate(df_ori.state):
   df_ori['state_abbr'][i], df_ori['country_abbr'][i] = Find_State_Country(state_ori)
          
 with st.expander("Display the data"): 
-         st.dataframe(df_ori)
+         st.dataframe(df_ori.loc[:,[45,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,46,47,44]])
          
 #sidebar
 df_1=df_ori
