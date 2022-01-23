4113d20b83a9ef75cca574e190eabcf274be625e
maggiexio <53416822+maggiexio@users.noreply.github.com>
2022-01-22 19:59:13 -0500

    Update Capstone_DSA_Maggie.py

--- Capstone_DSA_Maggie.py
+++ Capstone_DSA_Maggie.py
@@ -129,11 +129,11 @@ df_1=df_1.query("rt_total>=@time_1 and rt_total<=@time_2")
 age_1, age_2 = st.sidebar.slider("Age range",  min(df_ori.age), max(df_ori.age), (min(df_ori.age), max(df_ori.age)))    
 df_1=df_1.query("age>=@age_1 and age<=@age_2")
 sex=['All']
-sex.append(df_1['gender'].drop_duplicates())
-
+sex1=df_1['gender'].drop_duplicates()
+sex=pd.concat(sex, sex1)
 mode=['All']
-mode.append(df_1['home_computer'].drop_duplicates())
-
+mode1=df_1['home_computer'].drop_duplicates()
+mode=pd.concat(mode, mode1)
 sex_choice = st.sidebar.selectbox('Select gender:', sex)
 if sex_choice != "All":
   df_1=df_1.query("gender==@sex_choice")
