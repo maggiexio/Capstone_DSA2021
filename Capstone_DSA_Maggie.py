c368e95d0c7c4ee15a70b9306d29c9290a627f54
maggiexio <53416822+maggiexio@users.noreply.github.com>
2022-01-24 18:16:04 -0500

    Update Capstone_DSA_Maggie.py

--- Capstone_DSA_Maggie.py
+++ Capstone_DSA_Maggie.py
@@ -101,7 +101,7 @@ with col11:
   title_1="Data Excursion"
   st.markdown(f'<h1 style="text-aligh: center;color: green;">{title_1}</h1>',unsafe_allow_html=True)
   subj_1="-- DSA Capstone Project"
-  st.markdown(f'<h2 style="text-aligh: right,color: green;">{subj_1}</h2>',unsafe_allow_html=True)
+  st.markdown(f'<h2 style="text-aligh: right;color: green;">{subj_1}</h2>',unsafe_allow_html=True)
   
 st.markdown("This Dashboard is designed for the capstone project of DSA 2021-2022")
 st.markdown ("By: Maggie Xiong") 
