c97414aa84c6f2ef38a325551543f774458ae30a
maggiexio <53416822+maggiexio@users.noreply.github.com>
2022-01-24 19:41:22 -0500

    Update Capstone_DSA_Maggie.py

--- Capstone_DSA_Maggie.py
+++ Capstone_DSA_Maggie.py
@@ -125,8 +125,13 @@ with col11:
         Please select which **state** data you want to view. 
         """)
       state_1=df_ori_1['state_abbr'].drop_duplicates()
+      default_state=['All']
+      state_1=default_state.extend(state_1)
       state_choice=st.multiselect("", state_1)
-      df_ori_2=df_ori_1.query("state_abbr in @state_choice")
+      if (default_state in state_choice):
+        df_ori_2=df_ori_1
+      else:
+        df_ori_2=df_ori_1.query("state_abbr in @state_choice")
       st.dataframe(df_ori_2)
          
 # Filters
