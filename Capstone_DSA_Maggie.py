d8a7b70d0ba97f25c07d90dc10d86fcc599e46ea
maggiexio <53416822+maggiexio@users.noreply.github.com>
2022-01-26 18:04:45 -0500

    Update Capstone_DSA_Maggie.py

--- Capstone_DSA_Maggie.py
+++ Capstone_DSA_Maggie.py
@@ -256,8 +256,8 @@ with col11:
   with st.expander("Histogram"):    
     fig_hist1=px.histogram(df_1, x='sum_score', color='gender', facet_col='home_computer', marginal='box')
     st.plotly_chart(fig_hist1, width=600, height=600)
-   with st.expander("Bar charts"):   
-    fig_bar1=px.histogram(df_1, y='sum_score', color='gender', facet_row='age_group')
+   with st.expander("Bar charts"): 
+    fig_bar1=px.bar(df_1, y='sum_score', color='gender', facet_row='age_group')
     st.plotly_chart(fig_bar1, width=600, height=600)
   with st.expander("Animation"):  
     fig_ani1=px.bar(df_1, x='age_group', animation_frame='state_abbr', color='gender')
