f1683f728d53bd553c6ef9fe3bb46b1946dd0d80
maggiexio <53416822+maggiexio@users.noreply.github.com>
2022-01-26 19:05:03 -0500

    Update Capstone_DSA_Maggie.py

--- Capstone_DSA_Maggie.py
+++ Capstone_DSA_Maggie.py
@@ -278,7 +278,7 @@ with col11:
   st.markdown(f'<h4 style="text-aligh: center;color: green;">{title_ch3}</h4>',unsafe_allow_html=True)
   with st.expander(""): 
     fig_scatter1=px.scatter_3d(df_1, y='sum_score', x='age', z='home_computer', color='gender', size='rt_scale', size_max=50)
-    st.plotly_chart(fig_scatter1,  use_container_width=True,, height=3000)
+    st.plotly_chart(fig_scatter1,  use_container_width=True, height=3000)
 
         
 #Ballon
